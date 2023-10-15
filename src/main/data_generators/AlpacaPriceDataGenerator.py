ca12e17e3e5ffa82421635dd47eb40bfcfba4031
Király Tamás <Tamas.Kiraly@loxon.eu>
2023-10-15 12:29:14 +0200

    Clean up AlpacaPriceDataGenerator

--- src/main/data_generators/AlpacaPriceDataGenerator.py
+++ src/main/data_generators/AlpacaPriceDataGenerator.py
@@ -30,35 +30,6 @@ class AlpacaPriceDataGenerator(PriceDataGeneratorBase):
                     self.current_data_window[bar['S']].drop(0)
         else:
             raise ValueError("Minute bar list is empty.")
-                    
-    ##NOTE: nem köll
-    #def load_prev_day_watchlist_data(self):
-    #    if self.recommended_sticker_list:
-    #        for symbol in self.recommended_sticker_list:
-    #            scanning_day = TimeFrame(amount=1, unit=TimeFrameUnit.Minute)
-    #            bars_request = StockBarsRequest(
-    #                symbol_or_symbols=symbol,
-    #                timeframe=scanning_day
-    #            )
-    #            
-    #            self.prev_day_data = self.historical_data_client.get_stock_bars(bars_request).df
-    #            self.prev_day_sticker_stats = self._calculate_prev_day_sticker_stats()
-    #    else:
-    #        raise ValueError("Recommended sticker list is empty.")
-    #    
-    #def _calculate_prev_day_sticker_stats(self) -> dict:
-    #    high = self.prev_day_data['high']
-    #    low = self.prev_day_data['low']
-    #    close = self.prev_day_data['close']
-    #    volume = self.prev_day_data['volume']
-    #    price_range_perc = (high.max() - low.min()) / close.mean() * 100
-    #    volume_range_ratio = (volume.max() - volume.min()) / volume.mean()
-    #    return {
-    #            'avg_close': close.mean(),
-    #            'avg_volume': volume.mean(),
-    #            'price_range_perc': price_range_perc,
-    #            'volume_range_ratio': volume_range_ratio
-    #            }
                             
     def load_watchlist_daily_price_data(self):
         if self.recommended_sticker_list is not None:
@@ -77,8 +48,6 @@ class AlpacaPriceDataGenerator(PriceDataGeneratorBase):
                     'price_range_perc': (max_high - min_low) / avg_close * 100,
                     'volume_range_ratio': (max_volume - min_volume) / avg_volume
                     }
-                
-                ''' Here is a place, where a-priori constraints like price boundaries could be applied! '''
                 
                 self.sticker_data['stickers'][symbol]['trading_day_data'] = pd.DataFrame(self.current_data_window[symbol])
                 self.sticker_data['stickers'][symbol]['trading_day_sticker_stats'] = pd.DataFrame.from_dict(self.trading_day_sticker_stats)
