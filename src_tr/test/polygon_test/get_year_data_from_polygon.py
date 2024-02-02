40ebfa47d6c9bac13b97553aa77f38949d4752cb
tamkiraly <tamaskiraly90@gmail.com>
2024-02-02 20:13:31 +0100

    split_data_by_day.py bugfix és refaktorálás, get_year_data_from_polygon.py plusz checkek error response-ra

--- src_tr/test/polygon_test/get_year_data_from_polygon.py
+++ src_tr/test/polygon_test/get_year_data_from_polygon.py
@@ -8,6 +8,7 @@ import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 import json
+import logging
 
 from src_tr.test.polygon_test.split_data_by_day import split_data_by_day
 
@@ -16,21 +17,29 @@ SYMBOL_CSV_PATH = os.environ["SYMBOL_CSV_PATH"]
 nasdaq_symbols = get_nasdaq_symbols(file_path=SYMBOL_CSV_PATH)
 
 def download_aggs(symbol: str, from_: str, to: str) -> List[dict]:
-    if symbol is not None and isinstance(symbol, str) and '/' in symbol:
-        symbol = symbol.replace("/", ".")
-    url = f"https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/minute/{from_}/{to}?adjusted=true&sort=asc&limit=50000&apiKey=Rdcy29x7wooeMAlqKIsnTfDiHOaz0J_d"
-    session = requests.Session()
-    retry = Retry(connect=3, backoff_factor=60)
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
+    if symbol is not None and isinstance(symbol, str):
+        if '/' in symbol:
+            symbol = symbol.replace("/", ".")
+        url = f"https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/minute/{from_}/{to}?adjusted=true&sort=asc&limit=50000&apiKey=Rdcy29x7wooeMAlqKIsnTfDiHOaz0J_d"
+        session = requests.Session()
+        retry = Retry(connect=3, backoff_factor=60)
+        adapter = HTTPAdapter(max_retries=retry)
+        session.mount('http://', adapter)
+        session.mount('https://', adapter)
 
-    data = session.get(url).text
-    data_dict = json.loads(data)
-    if data_dict is not None and 'results' in data_dict:
-        return data_dict['results']
+        response = session.get(url)
+        if response.status_code == 200:
+            data_dict = json.loads(response.text)
+            if data_dict is not None and 'results' in data_dict:
+                return data_dict['results']
+            else:
+                logging.error(f"No available bar data for %s between %s and %s", symbol, from_, to)
+                return None
+        else:
+            no_api_key_url = url.split("&apiKey")
+            logging.error(f"%s %s at URL: %s", response.status_code, response.reason, no_api_key_url[0])
+            return None
     else:
-        print(f"No available bar data for {symbol} between {from_} and {to}")
         return None
 
 def format(minute_data, symbol):
@@ -56,7 +65,7 @@ def get_bar_data(symbol: str, from_: str, to: str):
             df['symbol'] = symbol
             return df
         except Exception as ex:
-            print(f'Error: {ex}')
+            logging.error(f"%s in file: %s, line %s", ex, __file__)
     else:
         return None
 
@@ -65,8 +74,9 @@ def create_csvs_by_symbol(symbol_list: list, from_: str, to: str):
     for symbol in symbol_list:
         if counter < 5:
             symbol_df = get_bar_data(symbol, from_, to)
-            split_data_by_day(symbol_df)
-            counter+=1
+            if symbol_df is not None and not symbol_df.empty:
+                split_data_by_day(symbol_df)
+                counter+=1
         else:
             time.sleep(61)
             counter = 0
--- src_tr/test/polygon_test/split_data_by_day.py
+++ src_tr/test/polygon_test/split_data_by_day.py
@@ -1,60 +1,41 @@
 import os
 import pandas as pd
+import numpy as np
 from datetime import datetime
-
-def split_data_by_day_Kovi_original():
-    full_symbol_df = pd.read_csv('F:/tradingActionExperiments_database/input/polygon/bar_dfs/AAPL_2023-01-26_2023-04-03.csv')
-    full_symbol_df['timestamp'] = pd.DatetimeIndex(pd.to_datetime(full_symbol_df['timestamp'], utc=True)).tz_convert('US/Eastern')
-
-    dates = full_symbol_df['timestamp'].dt.date.unique()
-
-
-    for i, date in enumerate(dates[:-2]):
-        daily_df = full_symbol_df[(full_symbol_df['timestamp'].dt.date > dates[i]) &
-                                (full_symbol_df['timestamp'].dt.date < dates[i+2])].copy()
-
-
-        trading_day_df: pd.DataFrame = daily_df[daily_df['timestamp'] > pd.to_datetime(dates[i].strftime('%Y-%m-%d') + ' ' + '09:30:00', utc=True).tz_convert('US/Eastern')]
-
-
-        date_str = date.strftime('%Y_%m_%d')
-        out_symbol_name = daily_df.symbol.unique()[0]
-        trading_day_df.set_index('timestamp', inplace=True)
-        trading_day_df.drop('symbol', inplace=True, axis=1)
-        if f'stock_prices_for_{date_str}' not in os.listdir('F:/tradingActionExperiments_database/input/polygon/daywise_database'):
-            os.mkdir(f'F:/tradingActionExperiments_database/input/polygon/daywise_database/stock_prices_for_{date_str}')
-        trading_day_df.to_csv(f'F:/tradingActionExperiments_database/input/polygon/daywise_database/stock_prices_for_{date_str}/{out_symbol_name}.csv')
         
 def split_data_by_day(full_symbol_df: pd.DataFrame, filter_length=None):
     #TODO: configból
     output_folder_path = "/home/tamkiraly/Development/tradingActionExperiments/src_tr/test/polygon_test/daily_bars"
 
+    #NOTE: erre csak a fájlból konvertáláshoz van szükségünk, mert abban nincs timezone infó
     full_symbol_df['timestamp'] = pd.DatetimeIndex(pd.to_datetime(full_symbol_df['timestamp'], utc=True)).tz_convert('US/Eastern')
-    dates = full_symbol_df['timestamp'].dt.date.unique()
+    dates = np.sort(full_symbol_df['timestamp'].dt.date.unique())
 
-    for i, date in enumerate(dates[:-2]):
-        daily_df = full_symbol_df[(full_symbol_df['timestamp'].dt.date > dates[i]) &
-                                (full_symbol_df['timestamp'].dt.date < dates[i+2])].copy()
-
-        trading_day_df = daily_df[daily_df['timestamp'] > pd.to_datetime(dates[i].strftime('%Y-%m-%d') + ' ' + '09:30:00', utc=True).tz_convert('US/Eastern')]
-
-        date_str = date.strftime('%Y_%m_%d')
-        out_symbol_name = daily_df.symbol.unique()[0]
-        trading_day_df.set_index('timestamp', inplace=True)
-        trading_day_df.drop('symbol', inplace=True, axis=1)
-        if date_str not in os.listdir(output_folder_path):
-            os.mkdir(f"{output_folder_path}/{date_str}")
-            
-        start_time = datetime.strptime('09:30:00', '%H:%M:%S').time()
-        end_time = datetime.strptime('16:00:00', '%H:%M:%S').time()
-        time_filtered_df = trading_day_df.between_time(start_time, end_time)
+    for date in dates:
+        daily_df = full_symbol_df[(full_symbol_df['timestamp'].dt.date == date)].copy()
+        symbol = daily_df.symbol.unique()
         
-        if filter_length is None or (filter_length > 0 and time_filtered_df.shape[0] >= filter_length):
-            time_filtered_df.to_csv(f"{output_folder_path}/{date_str}/{out_symbol_name}.csv")
+        if symbol.size > 1:
+            raise ValueError(f"Daily DataFrame contains more than 1 symbol: {symbol.tolist()}")
+        else:
+            symbol = symbol[0]
+            date_str = date.strftime('%Y_%m_%d')
+            
+            daily_df.set_index('timestamp', inplace=True)
+            start_time = datetime.strptime('09:30:00', '%H:%M:%S').time()
+            end_time = datetime.strptime('16:00:00', '%H:%M:%S').time()
+            daily_df = daily_df.between_time(start_time, end_time)
+            daily_df.drop('symbol', inplace=True, axis=1)
+            
+            if date_str not in os.listdir(output_folder_path):
+                os.mkdir(f"{output_folder_path}/{date_str}")
+            
+            if filter_length is None or (filter_length > 0 and daily_df.shape[0] >= filter_length):
+                daily_df.to_csv(f"{output_folder_path}/{date_str}/{symbol}.csv")
         
 def split_downloaded_data():
     #TODO: configból
-    csv_dir = "/home/tamkiraly/Development/tradingActionExperiments/src_tr/test/polygon_test/bar_data_csvs"
+    csv_dir = "/home/tamkiraly/Development/tradingActionExperiments/src_tr/test/polygon_test/full_bar_csvs"
     for filename in os.listdir(csv_dir):
         csv_file_path = os.path.join(csv_dir, filename)
         if os.path.isfile(csv_file_path):
